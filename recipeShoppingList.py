8412aef0469aad94f5b97ae91a9e45d813fd58c2
josh <whitehead.josh98@gmail.com>
2023-12-28 22:26:48 -0700

    app

---
+++ recipeShoppingList.py
@@ -0,0 +1,60 @@
+import pandas as pd 
+import numpy as np 
+
+
+ingredientTypes2 = {}
+shoppingList = {}
+recipes = ['recipe 1','recipe 2']
+
+for rec in recipes:
+    recipe = pd.read_excel('shoppingTest.xlsx',sheet_name=rec)
+    ingredientTypes = {
+    'dairy':[],
+    'produce':[],
+    'seasonings':[],
+    'grains':[],
+    'protein':[],
+    'misc':[]
+    }
+    for col in recipe.columns:
+        try:
+            ing = col.split()[0]
+            ingredientTypes[ing].append(list(recipe[col]))
+            ingredientTypes2[ing] = np.array(ingredientTypes[ing]).T
+        except:
+            pass
+for i in ingredientTypes2:
+    for u in ingredientTypes2[i]:
+        u = list(u)
+        while 'nan' in u:
+            u.remove('nan')
+        combine = ' '.join(u)
+        if i not in shoppingList:
+            shoppingList[i] = [combine]
+        else:
+            shoppingList[i].append(combine)
+lens = []
+for i in shoppingList:
+    shoppingList[i].sort(reverse=1)
+    lens.append(len(shoppingList[i]))
+# print(shopp|ingList)
+
+while len(set(lens)) > 1:
+    smallest = min(lens)
+    smallestIndx = lens.index(smallest)
+    keys = list(shoppingList.keys())
+    shoppingList[keys[smallestIndx]].append(None)
+    lens = []
+    for i in shoppingList:
+        lens.append(len(shoppingList[i]))
+
+
+
+dF = pd.DataFrame(shoppingList)
+count = 1
+x = ''
+for i in range(len(shoppingList)-1):
+    dF.insert(i+count,x,None)
+    count += 1
+    x += ' '
+dF.to_excel('test.xlsx',index=0)
